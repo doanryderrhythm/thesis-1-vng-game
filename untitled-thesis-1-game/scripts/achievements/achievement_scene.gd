80151c86bcf4979aaaa867e35fdbcff91b8fe452
Ryder <doanryder2005@gmail.com>
2026-04-21 13:45:38 +0700

    Created more visual experience

--- untitled-thesis-1-game/scripts/achievements/achievement_scene.gd
+++ untitled-thesis-1-game/scripts/achievements/achievement_scene.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var achievement_listener: AchievementListener = load("res://resources/achievements/achievement_listener.tres")
 
 func _ready() -> void:
+	RenderingServer.set_default_clear_color(Color(0.0, 0.092, 0.184, 1.0))
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
 	for ach in achievement_listener.achievements:
 		var ach_item: AchievementShownItem = achievement_item.instantiate()
 		ach_item.achievement = ach
